29e096aa55eb596ebf3de39088428df05d10bb8f
Patt Sookmark <pijiya@sookmark.com>
2024-01-09 10:05:07 -0700

    Add form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import store from './store1/configureStore';
+import AddTask from './components/AddTask';
 import Tasks from './components/Tasks';
 
 const App = () => {
   return (
     <div>
+      <AddTask />
       <Tasks />
     </div>
   );
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,29 +1,29 @@
-import React, { useState } from "react";
-import { addNewTask } from "../store/tasks";
-import { useDispatch } from "react-redux";
+import React, { useState } from 'react';
+import { addNewTask } from '../store1/tasks';
+import { useDispatch } from 'react-redux';
 
 const AddTask = () => {
-    const [task, setTask] = useState("");
-    const dispatch = useDispatch();
+  const [task, setTask] = useState('');
+  const dispatch = useDispatch();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        dispatch(addNewTask({ task: task }));
-        setTask("");
-    };
-    return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type='text'
-                name='task'
-                placeholder='Enter new task...'
-                value={task}
-                onChange={(e) => setTask(e.target.value)}
-            />
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addNewTask({ task: task }));
+    setTask('');
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        name='task'
+        placeholder='Enter new task...'
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
 
-            <button type='submit'>Add Task</button>
-        </form>
-    );
+      <button type='submit'>Add Task</button>
+    </form>
+  );
 };
 
 export default AddTask;
